39c3c25a589e9ae0cb503a19dbf5182ae84e01cd
londes <wedozeez@gmail.com>
2022-02-17 16:04:23 -0500

    building a util to deal with scientific notation to solve euler problem 20

---
+++ non_scientific_notation.js
@@ -0,0 +1,24 @@
+// Utility intended to remove scientific notation from a number and return an actual integer for large numbers
+
+function toFixed(x) {
+    if (Math.abs(x) < 1.0) {
+        console.log("we\'re in the low side and x is: " + x);
+        let e = parseInt(x.toString().split('e-')[1]);
+        if (e) {
+            x *= Math.pow(10,e-1);
+            x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+        }
+    } else {
+        console.log("we\'re in the high side and x is: " + x);
+        let e = parseInt(x.toString().split('+')[1]);
+        console.log("and e is: " + e);
+        if (e < 20) {
+            e -= 20;
+            x /= Math.pow(10,e);
+            x += (new Array(e+1).join('0'));
+        }
+    }
+    return x;
+}
+
+console.log(toFixed(9.332621544394418e+157));
